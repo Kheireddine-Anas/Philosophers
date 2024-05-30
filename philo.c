a1ae3f83e4763df2cb3e8a9b7ab2958c9dfcf50f
Kheireddine-Anas <kheireddine.anas@gmail.com>
2024-05-30 19:37:08 +0100

    Before Ending

--- philo.c
+++ philo.c
@@ -1,34 +1,86 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
+#include "philoshophers.h"
 
-void	*func(void *arg)
+t_philos	*init(t_data *data, char **argv)
 {
-	while (1)
+	int			i;
+	t_philos	*philo;
+
+	i = -1;
+	data->n_philos = ft_atoi(argv[1]);
+	data->t_die = ft_atoi(argv[2]);
+	data->t_eat = ft_atoi(argv[3]);
+	data->t_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		data->n_time = ft_atoi(argv[5]);
+	philo = malloc(sizeof(t_philos) * data->n_philos);
+	gettimeofday(&data->tm, NULL);
+	while (++i < data->n_philos)
 	{
-		printf("L");
-		sleep((unsigned int)0.8);
-	}	
-	
-	return NULL;
-}
-void	*func1(void *arg)
-{
-	while (1)
-	{
-		printf("M");
-		sleep((unsigned int)0.8);
+		philo[i].id = i + 1;
+		philo[i].right_fork = i;
+		philo[i].left_fork = i + 1;
+		if (i == data->n_philos - 1)
+			philo[i].left_fork = 0;
+		philo[i].spcf = data;
+		philo[i].nm_eat = 0;
+		philo[i].lst_meal = get_time(data);
+		pthread_mutex_init(&data->forks[i], NULL);
 	}
-	return NULL;
+	return (philo);
 }
 
-int	main()
+void	repetition(void *arg)
 {
-	pthread_t	thread1, thread2;
-	pthread_create(&thread1, NULL, &func, NULL);
-	pthread_create(&thread2, NULL, &func1, NULL);
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
+	t_philos	*philo;
+
+	philo = (t_philos *)arg;
+	if (philo->id % 2 != 0)
+	{
+		ft_printer(philo, "is thinking");
+		ft_usleep(philo->spcf->t_eat, philo);
+	}
+	while (1)
+	{
+		if (check_life(philo, 0))
+			return ;
+		eating_cycle(philo);
+		ft_printer(philo, "is sleeping");
+		ft_usleep(philo->spcf->t_sleep, philo);
+		ft_printer(philo, "is thinking");
+	}
+}
+
+void	thread_make(t_philos *philo)
+{
+	int	i;
+
+	pthread_mutex_init(&philo->spcf->printer, 0);
+	pthread_mutex_init(&philo->spcf->meals, 0);
+	pthread_mutex_init(&philo->spcf->death, 0);
+	philo->spcf->life = 0;
+	i = 0;
+	while (i < philo->spcf->n_philos)
+	{
+		pthread_mutex_init(&philo->spcf->forks[i], 0);
+		i++;
+	}
+	i = 0;
+	while (i < philo->spcf->n_philos)
+	{
+		pthread_create(&philo[i].thrd, NULL, (void *)repetition, &philo[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	t_philos	*philo;
+
+	philo = NULL;
+	checking(argc, argv);
+	data.forks = malloc(sizeof(pthread_mutex_t) * ft_atoi(argv[1]));
+	philo = init(&data, argv);
+	thread_make(philo);
+	ending_cycle(philo, argv);
 }
--- philoshophers.h
+++ philoshophers.h
@@ -4,25 +4,44 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct 	s_philos
+typedef struct s_data
 {
-	int					nm_eat;
-	int					id;
-	pthread_mutex_t		left_fork;
-	pthread_mutex_t		*right_fork;
-}						t_philos;
-
-typedef struct	s_data
-{
-	int			n_philos;
-	t_philos	*philo;
-	int			t_die;
-	int			t_eat;
-	int			t_sleep;
-	int			n_time;
+	int				n_philos;
+	int				life;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				n_time;
+	pthread_mutex_t	printer;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	death;
+	pthread_mutex_t	meals;
+	struct timeval	tm;
 }				t_data;
+
+typedef struct s_philos
+{
+	int			nm_eat;
+	int			id;
+	int			left_fork;
+	int			right_fork;
+	int			lst_meal;
+	pthread_t	thrd;
+	t_data		*spcf;
+}			t_philos;
+
+long long	get_time(t_data *data);
+int			ft_atoi(char *str);
+int			check_life(t_philos *philo, int val);
+void		checking_one(int argc, char **argv);
+void		checking(int argc, char **argv);
+void		ft_usleep(int time, t_philos *philo);
+void		ft_printer(t_philos *philo, char *str);
+void		ending_cycle(t_philos *philo, char **argv);
+void		eating_cycle(t_philos *philo);
 
 #endif
